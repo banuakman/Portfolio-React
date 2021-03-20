d8e6caa1de96fb116806ebeecbfc3131aaf5395a
banuakman <banu@asterya.com>
2021-03-19 22:46:44 -0400

    Create Particle Effects With tsparticles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
+import Wrapper from "./components/Wrapper";
 import MainContainer from "./components/MainContainer";
 
-const App = () => <MainContainer />;
+const App = () => (
+  <Wrapper>
+    <MainContainer />;
+  </Wrapper>
+);
 
 export default App;
