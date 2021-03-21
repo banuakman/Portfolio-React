650f5522cf260fb5149c179fd0303646a54c4d53
banuakman <banu@asterya.com>
2021-03-20 20:28:12 -0400

    add some styling to home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,14 +2,25 @@ import React from "react";
 import Background from "../Particles";
 import "../styles/Home.css";
 
+const dStyle = {
+  color: "#FF4048",
+};
 
 const Home = () => (
   <>
     <Background />
     <div className="welcome text-center">
-      <h1 className="display-2 hello">Hello.</h1>
-      <h2 className="display-4">I am Banu Akman.</h2>
-      <p className="lead">I am a full-stack developer.</p>
+      <h1 className="display-2 hello">
+        Hello<span style={dStyle}>.</span>
+      </h1>
+      <h2 className="display-4">
+        I am Banu Akman<span style={dStyle}>.</span>
+      </h2>
+      <p className="lead">
+        I am a full-stack developer<span style={dStyle}>.</span> I drink coffee
+        <span style={dStyle}>.</span> I write code<span style={dStyle}>.</span> I
+        improve my skills and design every day<span style={dStyle}>.</span>
+      </p>
     </div>
   </>
 );
