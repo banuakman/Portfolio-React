c6f3de26300078733023b48808f4586a8532a177
banuakman <banu@asterya.com>
2021-03-20 22:14:34 -0400

    add contact page & more styling on home page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,20 +1,39 @@
 import React from "react";
+import Background from "../Particles";
+import "../styles/Contact.css";
+
+
+const dStyle = {
+  color: "#FF4048",
+};
 
 const Contact = () => (
-  <div>
-    <h1>Contact Page</h1>
-    <p>
-      Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis molestie urna.
-      Aliquam semper ultrices varius. Aliquam faucibus sit amet magna a ultrices. Aenean
-      pellentesque placerat lacus imperdiet efficitur. In felis nisl, luctus non ante euismod,
-      tincidunt bibendum mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-      posuere, eget tristique dui dapibus. Maecenas fermentum elementum faucibus. Quisque nec metus
-      vestibulum, egestas massa eu, sollicitudin ipsum. Nulla facilisi. Sed ut erat ligula. Nam
-      tincidunt nunc in nibh dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-      conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at rhoncus. Etiam vel
-      condimentum magna, quis tempor nulla.
-    </p>
-  </div>
+  <>
+    <Background />
+    <div className="contact text-center">
+      <h1 className="display-3 hello">
+        Contact<span style={dStyle}>.</span>
+      </h1>
+      <p className="lead">
+        <a href="mailto:banu@breative.net" style={{ color: "white" }}>
+          Email: banu@breative.net
+        </a>
+      </p>
+      <p className="lead">
+        <a href="https://github.com/banuakman" style={{ color: "white" }}>
+          Github: github.com/banuakman
+        </a>
+      </p>
+      <p className="lead">
+        <a
+          href="https://www.linkedin.com/in/banuakman/"
+          style={{ color: "white" }}
+        >
+          LinkedIn: linkedin.com/in/banuakman/
+        </a>
+      </p>
+    </div>
+  </>
 );
 
 export default Contact;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -10,16 +10,14 @@ const Home = () => (
   <>
     <Background />
     <div className="welcome text-center">
-      <h1 className="display-2 hello">
+      <h1 className="display-3 hello">
         Hello<span style={dStyle}>.</span>
       </h1>
       <h2 className="display-4">
         I am Banu Akman<span style={dStyle}>.</span>
       </h2>
       <p className="lead">
-        I am a full-stack developer<span style={dStyle}>.</span> I drink coffee
-        <span style={dStyle}>.</span> I write code<span style={dStyle}>.</span> I
-        improve my skills and design every day<span style={dStyle}>.</span>
+        I am a full-stack developer and a passionate designer<span style={dStyle}>.</span> <br></br>I drink coffee<span style={dStyle}>.</span> I write code<span style={dStyle}>.</span> I improve my design and skills every day<span style={dStyle}>.</span>
       </p>
     </div>
   </>
