f553edcc7b0d7a9d30d79d78b312a9b810d58dd1
banuakman <banu@asterya.com>
2021-03-20 22:30:28 -0400

    add more styling

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,32 +4,31 @@ import "../styles/Contact.css";
 
 
 const dStyle = {
-  color: "#FF4048",
+  color: "#E8C3B9",
 };
 
 const Contact = () => (
   <>
     <Background />
     <div className="contact text-center">
-      <h1 className="display-3 hello">
+      <h1 className="display-3 contacttitle">
         Contact<span style={dStyle}>.</span>
       </h1>
       <p className="lead">
-        <a href="mailto:banu@breative.net" style={{ color: "white" }}>
-          Email: banu@breative.net
+        <a href="mailto:banu@breative.net" style={{ color: "#1A1A1A" }}>
+          banu@breative.net
         </a>
       </p>
       <p className="lead">
-        <a href="https://github.com/banuakman" style={{ color: "white" }}>
-          Github: github.com/banuakman
+        <a href="https://github.com/banuakman" style={{ color: "#1A1A1A" }}>
+          github.com/banuakman
         </a>
       </p>
       <p className="lead">
         <a
-          href="https://www.linkedin.com/in/banuakman/"
-          style={{ color: "white" }}
+          href="https://www.linkedin.com/in/banuakman/" style={{ color: "#1A1A1A" }}
         >
-          LinkedIn: linkedin.com/in/banuakman/
+          linkedin.com/in/banuakman/
         </a>
       </p>
     </div>
